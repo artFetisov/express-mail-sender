5b85ce6b19a76abcf8a17753e0d8af0adf4642ce
artFetisov <fetishfestoff@mail.ru>
2023-01-16 17:32:28 +0300

    feach: cors middleware

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express, {Express, Request, Response} from 'express'
+import {corsMiddleware} from "./cors.middleware";
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const mailService = require('./mail-service')
 const app: Express = express()
 const PORT = process.env.PORT || 5000
 
+app.use(corsMiddleware)
 app.use(cors())
 app.use(bodyParser.json())
 
