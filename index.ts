1396a1e98b2e7ed37bb01f74a25493486295d247
artFetisov <fetishfestoff@mail.ru>
2023-01-18 15:18:50 +0300

    refactor

--- index.ts
+++ index.ts
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 5000
 app.use(corsMiddleware)
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
