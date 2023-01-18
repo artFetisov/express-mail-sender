9fe8df15de4efd95b291cddc03e8fb2fbf35aa14
artFetisov <fetishfestoff@mail.ru>
2023-01-18 15:09:38 +0300

    refactor

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { IMailDTO } from './dto'
+import {IMailDTO} from './dto'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import express, { Express, Request, Response } from 'express'
-import { corsMiddleware } from './cors.middleware'
+import express, {Express, Request, Response} from 'express'
+import {corsMiddleware} from './cors.middleware'
 
 dotenv.config()
 
@@ -17,25 +17,25 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send({
-    art: process.env.SMTP_SERVICE,
-    user: process.env.SMTP_USER,
-  })
+    res.send({
+        art: process.env.SMTP_SERVICE,
+        user: process.env.SMTP_USER,
+    })
 })
 
 app.post('/send-mail', async (req: Request, res: Response) => {
-  try {
-    const data: IMailDTO = req.body
+    try {
+        const data: IMailDTO = req.body
 
-    res.send('message is send!')
+        res.status(200).send(data)
 
-    await mailService.sendEmail(data)
-  } catch (error) {
-    console.log('ERROR')
-    console.log(error)
-  }
+        // await mailService.sendEmail(data)
+    } catch (error) {
+        console.log('ERROR')
+        console.log(error)
+    }
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
